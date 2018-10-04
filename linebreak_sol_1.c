f942395aeddfbcf0ccc2f99e49da4b12b1fa6db9
Diego <salas.diego@pucp.pe>
2018-10-03 19:43:00 -0500

    Linebreak test

---
+++ linebreak_sol_1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	char c;
+	scanf("%c",&c);
+	printf("%c\n",c);
+	scanf("\n%c",&c);
+	printf("%c\n",c);
+	return 0;
+}
---
+++ linebreak_sol_weird.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	char c;
+	scanf("%c\n",&c);
+	printf("%c\n",c);
+	scanf("%c",&c);
+	printf("%c\n",c);
+	return 0;
+}
